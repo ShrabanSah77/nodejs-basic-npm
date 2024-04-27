9737142532b4a05114f5f555c416d718534bccc1
ShrabanSah77 <shrabanshah77@gmail.com>
2024-04-28 00:43:47 +0545

    update qrcode

--- qrCode.js
+++ qrCode.js
@@ -1,5 +1,14 @@
-const QRCode = require('qrcode')
+const QRCode = require("qrcode");
+const http = require("http");
 
-QRCode.toString('I am a pony!',{type:'terminal'}, function (err, url) {
-  console.log(url)
-})
+http
+  .createServer(async (req, res) => {
+    console.log(req.url);
+    const qr = await QRCode.toDataURL("https://github.com/ShrabanSah77");
+    const imgSrc = `<img src="${qr}"/>`;
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(imgSrc);
+  })
+  .listen(5000);
+console.log("App running on http://localhost:5000");
